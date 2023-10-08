2be096dbf5a84a3a6b236726ba250ccac9e508c9
Anthony Fu <anthonyfu117@hotmail.com>
2023-10-09 00:44:02 +0800

    chore: update

--- app.vue
+++ app.vue
@@ -13,15 +13,17 @@ await ensureDataFetch()
 <template>
   <div v-if="errorInfo" grid h-full w-full place-content-center whitespace-pre-line>
     <div text-xl font-bold text-red5>
-      Failed to load <code bg-red:5 px2 py1 rounded>./eslint.config.js</code><br>
+      Failed to load <code rounded bg-red:5 px2 py1>./eslint.config.js</code><br>
     </div>
-   
-    <div text-red font-mono text-lg>{{ errorInfo.error }}</div>
 
-    <div op50 mt-5>
+    <div text-lg font-mono text-red>
+      {{ errorInfo.error }}
+    </div>
+
+    <div mt-5 op50>
       Note that
       <a href="https://github.com/antfu/eslint-flat-config-viewer" target="_blank" hover:underline>config viewer</a>
-      only works with the <a href="https://eslint.org/docs/latest/use/configure/configuration-files-new" target="_blank" hover:underline font-bold>new flat config format</a>.
+      only works with the <a href="https://eslint.org/docs/latest/use/configure/configuration-files-new" target="_blank" font-bold hover:underline>new flat config format</a>.
     </div>
   </div>
   <div v-else px14 py10>
