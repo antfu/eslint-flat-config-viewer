3afb3f25b53ef39448b3dc64881ffb482ff180f2
Anthony Fu <anthonyfu117@hotmail.com>
2024-01-15 12:17:26 +0100

    fix: allow any cors

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -26,8 +26,15 @@ export default defineNuxtConfig({
       dir: './dist',
     },
     routeRules: {
-      '**': {
+      '/**': {
         cors: true,
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+          'Access-Control-Allow-Methods': '*',
+          'Access-Control-Allow-Credentials': 'true',
+          'Access-Control-Allow-Headers': '*',
+          'Access-Control-Expose-Headers': '*',
+        },
       },
     },
     prerender: {
