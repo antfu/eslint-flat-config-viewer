f4446a4b71fd1e68699cf518f220463d332b2e5b
Anthony Fu <anthonyfu117@hotmail.com>
2024-01-15 10:59:21 +0100

    fix: allow been controlled by env

--- bin.mjs
+++ bin.mjs
@@ -4,10 +4,12 @@ import process from 'node:process'
 import open from 'open'
 import { getPort } from 'get-port-please'
 
-process.env.HOST = '127.0.0.1'
-process.env.PORT = await getPort({ port: 7777 })
+process.env.HOST = process.env.HOST || '127.0.0.1'
+process.env.PORT = process.env.PORT || await getPort({ port: 7777 })
 
 await Promise.all([
   import('./dist/server/index.mjs'),
-  open(`http://localhost:${process.env.PORT}`),
+  process.env.NO_OPEN
+    ? undefined
+    : open(`http://localhost:${process.env.PORT}`),
 ])
